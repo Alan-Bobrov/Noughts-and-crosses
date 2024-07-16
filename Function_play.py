bc7f13df90f1160c4b78ab09a04860319735ab3f
Alan-Bobrov <159445827+Alan-Bobrov@users.noreply.github.com>
2024-07-16 10:53:44 +0300

    Add files via upload

---
+++ Function_play.py
@@ -0,0 +1,29 @@
+from Class_Field import Field
+
+def play():
+    winner = ""
+    play_field = Field()
+    print("Now you are playing noughts and crosses where you play for the 'X' and start first,")
+    print("and your opponent is a bot that goes to a random field")
+    print("Let's find out if you can beat randomness!")
+    play_field.print_field()
+    while True:
+        play_field.move()
+        winner = play_field.test_end()
+        if winner != "":
+            break
+        play_field.move(bot=True)
+        play_field.print_field()
+        winner = play_field.test_end()
+        if winner != "":
+            break
+    play_field.print_field()
+    if winner == "X":
+        print("You win!")
+        print("--------")
+    elif winner == "O":
+        print("You lose!")
+        print("---------")
+    else:
+        print("Draw")
+        print("----")
---
+++ Noughts and crosses play.py
@@ -0,0 +1,9 @@
+from Function_play import play
+
+while True:
+    play()
+    end = input("You will be play more? (Yes or No, if you write somethihg another a will consider it as No):").lower()
+    if end == "yes":
+        pass
+    else:
+        break
